f41ebc106298b45b45cea1cefb52f78b585c0048
Tamas Nepusz <ntamas@gmail.com>
2019-11-19 20:14:47 +0100

    added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """Setup script for the aio_usb_hotplug package."""
 
 from glob import glob
-from os.path import basename, splitext
+from os.path import abspath, basename, dirname, join, splitext
 from setuptools import setup, find_packages
 
 requires = [
@@ -15,11 +15,18 @@ extras_require = {
     "dev": ["curio>=0.9", "pytest>=5.2.4", "pytest-cov>=2.0.1", "trio>=0.13.0"]
 }
 
+this_directory = abspath(dirname(__file__))
+
 __version__ = None
-exec(open("src/aio_usb_hotplug/version.py").read())
+exec(open(join(this_directory, "src", "aio_usb_hotplug", "version.py")).read())
+
+with open(join(this_directory, "README.md")) as f:
+    long_description = f.read()
 
 setup(
     name="aio_usb_hotplug",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version=__version__,
     author=u"Tam\u00e1s Nepusz",
     author_email="ntamas@gmail.com",
