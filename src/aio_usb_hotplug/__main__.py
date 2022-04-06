d577fa32fda6660b43945ad1e0fd56b505526ef3
Tamas Nepusz <ntamas@gmail.com>
2022-04-07 01:54:16 +0200

    fix: fixed __main__.py with new events() function in HotplugDetector

--- src/aio_usb_hotplug/__main__.py
+++ src/aio_usb_hotplug/__main__.py
@@ -5,8 +5,8 @@ from anyio import run
 
 async def main():
     detector = HotplugDetector.for_device(vid="1050", pid="0407")
-    async for event in detector.run():
-        print("{0.type.value} device {0.key}".format(event))
+    async for event in detector.events():
+        print(f"{event.type.value} device {event.key}")
 
 
 if __name__ == "__main__":
