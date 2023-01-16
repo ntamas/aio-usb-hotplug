41dc44a86099ea5e32a2df368e84012b82039f5d
Tamas Nepusz <ntamas@gmail.com>
2023-01-16 21:09:26 +0100

    feat: export HotplugEvent and HotplugEventType

--- src/aio_usb_hotplug/__init__.py
+++ src/aio_usb_hotplug/__init__.py
@@ -1,6 +1,13 @@
 from .backends.autodetect import choose_backend
 from .backends.base import Device
 from .errors import NoBackendError
-from .task import HotplugDetector
+from .task import HotplugDetector, HotplugEvent, HotplugEventType
 
-__all__ = ("choose_backend", "Device", "HotplugDetector", "NoBackendError")
+__all__ = (
+    "choose_backend",
+    "Device",
+    "HotplugDetector",
+    "HotplugEvent",
+    "HotplugEventType",
+    "NoBackendError",
+)
